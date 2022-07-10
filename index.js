52f4818b5e0c2b6c04440d53cec693e8349aa253
strudelPie <edward.strudwick1@gmail.com>
2022-07-10 20:32:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http')
+var fs = require('fs');
+var url = require('url');
+
+const port = process.env.PORT || 3000
+
+http.createServer((req, res) => {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    
+    var route = filename === "./" ? './index.html' : filename;
+
+    fs.readFile(route, function(err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      } 
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  }).listen(port);
+
+
